55d1d213dc13983e458d4baac62f7ac17edb52fc
Luca <luc.avolio@stud.uniroma3.it>
2016-06-10 10:13:33 +0200

    FolderReader

---
+++ src/it/uniroma3/reader/FolderReader.java
@@ -0,0 +1,15 @@
+package it.uniroma3.reader;
+
+import java.io.File;
+
+public class FolderReader {
+	public static void listFilesForFolder(final File folder) {
+	    for (final File fileEntry : folder.listFiles()) {
+	        if (fileEntry.isDirectory()) {
+	            listFilesForFolder(fileEntry);
+	        } else {
+	            System.out.println(fileEntry.getName());
+	        }
+	    }
+	}
+}
