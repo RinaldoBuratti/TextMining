8b406a509922eb64f415852c2f501ad5e5421cb8
RinaldoBuratti <rinaldoburatti@gmail.com>
2016-06-14 10:08:26 +0200

    modificata la classe jsonWriter

--- src/it/uniroma3/jsonWriter/JsonCreator.java
+++ src/it/uniroma3/jsonWriter/JsonCreator.java
@@ -3,6 +3,7 @@ package it.uniroma3.jsonWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.json.simple.JSONArray;
@@ -13,9 +14,6 @@ import it.uniroma3.agiw.path.PathMaker;
 public class JsonCreator {
 	JSONObject obj;
 	JSONObject ner;				//NER
-	JSONArray per;				//Lista di nomi per il campo PER del NER
-	JSONArray org;				//Lista di org per il campo ORG del NER
-	JSONArray loc;				//Lista di luoghi per il campo LOC del NER
 
 	JSONObject pattern;			//Pattern
 	JSONArray mail;				//Lista di email per il campo email del PATTERN
@@ -26,20 +24,12 @@ public class JsonCreator {
 		this.obj = new JSONObject();
 		this.ner = new JSONObject();
 		this.pattern = new JSONObject();
-		this.per = new JSONArray();
-		this.org = new JSONArray();
-		this.loc = new JSONArray();
 		this.mail = new JSONArray();
 		this.tel = new JSONArray();
 		this.name = new JSONArray();
 		this.obj.put("NER", ner);
 		this.obj.put("PATTERN", pattern);
 		this.obj.put("url", url);
-
-		//Definizione oggetto NER;
-		this.ner.put("PER", this.per);
-		this.ner.put("ORG", this.org);
-		this.ner.put("LOC", this.loc);
 
 		//Definizione oggetto PATTERN
 		this.pattern.put("email", this.mail);
@@ -50,30 +40,6 @@ public class JsonCreator {
 	public String toString() {
 		return this.obj.toJSONString();
 	};
-
-	/* Inserisco nel campo PER del NER una lista di PERSONE */
-	public boolean putPERInNER(ArrayList<String> persons) {		
-		if (this.per.addAll(persons))
-			return true;
-		else
-			return false;
-	};
-
-	/* Inserisco nel campo ORG del NER una lista di ORGANIZZAZIONI */
-	public boolean putORGInNER(ArrayList<String> orgs) {		
-		if (this.org.addAll(orgs))
-			return true;
-		else
-			return false;
-	}
-
-	/* Inserisco nel campo LOC del NER una lista di LUOGHI */
-	public boolean putLOCInNER(ArrayList<String> locs) {		
-		if (this.loc.addAll(locs))
-			return true;
-		else
-			return false;
-	}
 
 	/* Inserisco nel campo MAIL del PATTERN una lista di email */
 	public boolean putEMAILInPattern(ArrayList<String> emails) {		
@@ -99,11 +65,6 @@ public class JsonCreator {
 			return false;
 	}
 
-	/* Inserisco i valori nel NER */
-	public boolean pushIntoNER(ArrayList<String> per, ArrayList<String> org, ArrayList<String> loc) {
-		return this.putPERInNER(per) || this.putORGInNER(org) || this.putLOCInNER(loc);
-	}
-
 	/* Inserisco i valori nel PATTERN */
 	public boolean pushIntoPATTERN(ArrayList<String> email, ArrayList<String> tel, ArrayList<String> names) {
 		return this.putEMAILInPattern(email) || this.putTELInPattern(tel) || this.putNAMEInPattern(names);
@@ -125,5 +86,19 @@ public class JsonCreator {
 
 		return true;
 	}
-
+	
+	public boolean pushMapIntoNER(HashMap<String,ArrayList<String>> map) {
+		try {
+			for(String s: map.keySet()) {			
+				JSONArray atmp = new JSONArray();
+				atmp.addAll(map.get(s));
+				this.ner.put(s, atmp);
+			
+			}
+		} catch (Exception e) {
+			return false;
+		};
+		return true;
+		
+	}
 }
